3efa30478328fe1932923b25da42a9f3c05cb919
Davon Allen <davon92@gmail.com>
2025-08-12 00:58:30 -0400

    insertion sort

---
+++ Algorithms/InsertionSort/A_InsertionSort.cs
@@ -0,0 +1,22 @@
+﻿namespace Algorithms
+{
+    public class A_InsertionSort
+    {
+        public void InsertionSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
+                
+                while (j >= 0 && array[j] > key)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+
+                array[j + 1] = key;
+            }
+        }
+    }
+}
